cacc5c53f374a618d199adcc1631a889959eccf4
matteo.cappelletti <matteo.cappelletti@itsincom.it>
2026-04-23 09:38:31 +0200

    ADD: Review entity

---
+++ src/main/java/placesreviews/app/persistence/entity/Review.java
@@ -0,0 +1,92 @@
+package placesreviews.app.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "reviewer_id")
+    private User reviewer;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "approver_id")
+    private User approver;
+
+    @Column(name = "rating", nullable = false)
+    private int rating;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(User reviewer) {
+        this.reviewer = reviewer;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+
+    public User getApprover() {
+        return approver;
+    }
+
+    public void setApprover(User approver) {
+        this.approver = approver;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
