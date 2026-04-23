a42b5e4d1613c9e6814a1ffdfdbd8e2c5e76a74f
matteo.cappelletti <matteo.cappelletti@itsincom.it>
2026-04-23 10:05:10 +0200

    ADD: Place repository, added functions findByCity(string), findByUserId(int), findByNameContains(string)

---
+++ src/main/java/placesreviews/app/persistence/repository/PlaceRepository.java
@@ -0,0 +1,33 @@
+package placesreviews.app.persistence.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+import placesreviews.app.persistence.entity.Place;
+
+import java.util.List;
+import java.util.Map;
+
+@ApplicationScoped
+public class PlaceRepository implements PanacheRepositoryBase<Place, Integer> {
+
+    public List<Place> findByCity(String city) {
+        String query = "SELECT p FROM Place p WHERE p.city = :city";
+        return find(query, Map.of(
+                ":city", city
+        )).list();
+    }
+
+    public List<Place> findByUserId(int userId) {
+        String query = "SELECT p FROM Place p WHERE p.user.id = :userId";
+        return find(query, Map.of(
+                ":userId", userId
+        )).list();
+    }
+
+    public List<Place> findByNameContains(String contained) {
+        String query = "SELECT p FROM Place p WHERE p.name ilike :name";
+        return find(query, Map.of(
+                "name", ("%" + contained + "%")
+        )).list();
+    }
+}
