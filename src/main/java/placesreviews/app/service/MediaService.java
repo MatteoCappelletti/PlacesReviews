4082fd1354af071a5467db9813f93f3cf13b2c16
matteo.cappelletti <matteo.cappelletti@itsincom.it>
2026-04-29 15:06:44 +0200

    ADD: Media Service, with functions insert(int, string), findByPlaceId(int)

---
+++ src/main/java/placesreviews/app/service/MediaService.java
@@ -0,0 +1,53 @@
+package placesreviews.app.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import placesreviews.app.persistence.entity.Media;
+import placesreviews.app.persistence.entity.Place;
+import placesreviews.app.persistence.repository.MediaRepository;
+import placesreviews.app.persistence.repository.PlaceRepository;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@ApplicationScoped
+public class MediaService {
+
+    private final MediaRepository mediaRepository;
+
+    private final PlaceRepository placeRepository;
+
+    public MediaService(MediaRepository mediaRepository, PlaceRepository placeRepository) {
+        this.mediaRepository = mediaRepository;
+        this.placeRepository = placeRepository;
+    }
+
+    public Result insert(int placeId, String url) {
+        if (url == null || url.isBlank()) {
+            return Result.error("Url is required");
+        }
+
+        Place place = placeRepository.findById(placeId);
+
+        Media media = new Media();
+        media.setPlace(place);
+        media.setPath(url);
+        media.setCreatedAt(OffsetDateTime.now());
+        mediaRepository.persist(media);
+
+        return Result.success();
+    }
+
+    public List<Media> findByPlaceId(int placeId) {
+        return mediaRepository.findByPlaceId(placeId);
+    }
+
+    public record Result(boolean ok, String errorMessage) {
+        public static Result success() {
+            return new Result(true, null);
+        }
+
+        public static Result error(String message) {
+            return new Result(false, message);
+        }
+    }
+}
