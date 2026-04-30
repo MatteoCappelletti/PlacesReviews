0ea868ac3360c08a9c79302bf30cd1d9a9e059d5
matteo.cappelletti <matteo.cappelletti@itsincom.it>
2026-04-30 09:49:01 +0200

    ADD: Register qute html and Register resource

---
+++ src/main/java/placesreviews/app/web/RegisterResource.java
@@ -0,0 +1,51 @@
+package placesreviews.app.web;
+
+import io.quarkus.qute.Location;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.annotation.security.DenyAll;
+import jakarta.annotation.security.PermitAll;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import placesreviews.app.service.Result;
+import placesreviews.app.service.UserService;
+
+import java.net.URI;
+
+@Path("/register")
+@DenyAll
+public class RegisterResource {
+
+    private final Template registerTemplate;
+
+    private final UserService userService;
+
+    public RegisterResource(@Location("register.qute.html") Template registerTemplate, UserService userService) {
+        this.registerTemplate = registerTemplate;
+        this.userService = userService;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @PermitAll
+    public Response showPage() {
+        return Response.ok(registerTemplate.data("errorMessage", null)).build();
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_HTML)
+    @PermitAll
+    public Response doRegister(
+            @FormParam("username") String username,
+            @FormParam("password") String password,
+            @FormParam("verified_password") String verifiedPassword
+    ) {
+        Result result = userService.insert(username, password, verifiedPassword);
+
+        if (!result.ok()) {
+            return Response.ok(registerTemplate.data("errorMessage", result.errorMessage())).build();
+        }
+        return Response.seeOther(URI.create("/login")).build();
+    }
+}
